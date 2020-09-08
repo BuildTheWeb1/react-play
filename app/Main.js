66c8b28c3ab8a11b88781aa1e7a436075968037c
Clau C <build.the.web1@gmail.com>
2020-09-08 08:53:02 +0300

    removed fakeJson request & removed user posts compenent

--- app/Main.js
+++ app/Main.js
@@ -5,40 +5,12 @@ import ReactDOM from "react-dom";
 import Title from "./components/Title";
 import Timer from "./components/Timer";
 
-import { users } from "./Controller/FakeData";
-
 function MainComponent() {
   return (
     <div>
       <Title />
       <Timer />
-      <div className="table-responsive">
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Title</th>
-              <th scope="col">Body</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map(user => {
-              return <User id={user.id} title={user.title} body={user.body} key={user.id} />;
-            })}
-          </tbody>
-        </table>
-      </div>
     </div>
-  );
-}
-
-function User(props) {
-  return (
-    <tr>
-      <th scope="row">{props.userId}</th>
-      <td>{props.title}</td>
-      <td>{props.body}</td>
-    </tr>
   );
 }
 
